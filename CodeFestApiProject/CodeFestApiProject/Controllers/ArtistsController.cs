225cdfe6535a21d8e949b0c15f4aa31fe6b6a20f
Malin Hallgren <malin.m.hallgren@gmail.com>
2026-08-18 13:39:19 +0200

    Step 2, basic Artist model and controller

---
+++ CodeFestApiProject/CodeFestApiProject/Controllers/ArtistsController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CodeFestApiProject.Models;
+using System.Net;
+
+namespace CodeFestApiProject.Controllers
+{
+    internal class MockData
+    {
+        public static List<Artist> _artists { get; } = new List<Artist>
+        {
+            new Artist("the beatles"),
+            new Artist("queen"),
+            new Artist("led zeppelin"),
+            new Artist("pink floyd"),
+            new Artist("the rolling stones")
+        };
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArtistsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Artist>>> GetAll()
+        {
+            await Task.Delay(1000); // Simulate a delay for demonstration purposes
+            return Ok(MockData._artists);
+        }
+
+        [HttpGet]
+        [Route("name/{name}")]
+        public async Task<ActionResult<Artist>> GetByName([FromRoute] string name)
+        {
+            name = name.ToLower();
+            await Task.Delay(1000); // Simulate a delay for demonstration purposes
+            var artist = MockData._artists.FirstOrDefault(a => a.Name == name);
+
+            if (artist != null)
+            {
+                return Ok(artist);
+
+            }
+            else
+            {
+                return NoContent();
+            }
+        }
+
+        [HttpGet("name/query")]
+        public async Task<ActionResult<Artist>> GetByNameQ([FromQuery] string name)
+        {
+            name = name.ToLower();
+            await Task.Delay(1000); // Simulate a delay for demonstration purposes
+            var artist = MockData._artists.FirstOrDefault(a => a.Name == name);
+
+            if (artist != null)
+            {
+                return Ok(artist);
+
+            }
+            else
+            {
+                return NoContent();
+            }
+        }
+    }
+}
---
+++ CodeFestApiProject/CodeFestApiProject/Models/Artist.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CodeFestApiProject.Models
+{
+    [Index(nameof(Name), IsUnique = true)]
+    public class Artist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Artist(string name)
+        {
+            Name = name;
+        }
+    }
+}
